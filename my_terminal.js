9a3bf805df0932f6fcf6ea6590865efb09c06881
Pirran Salter <pirransalter@gmail.com>
2024-05-04 21:12:30 +0100

    another try at moving it around

--- my_terminal.js
+++ my_terminal.js
@@ -1,3 +1,27 @@
+const font = 'Doom';
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts'});
+figlet.preloadFonts([font], ready);
+ 
+
+function render(text) {
+    const cols = term.cols();
+    return trim(figlet.textSync(text, {
+        font: font,
+        width: cols,
+        whitespaceBreak: true
+    }));
+}
+
+function trim(str) {
+    return str.replace(/[\n\s]+$/, '');
+}
+
+
+const formatter = new Intl.ListFormat('en', {
+    style: 'long',
+    type: 'conjuction',
+})
+
 const command_list = Object.keys(commands);
 const help = formatter.format(command_list);
 
@@ -23,26 +47,4 @@ const commands = {
 };
 
 
-const font = 'Doom';
-figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts'});
-figlet.preloadFonts([font], ready);
- 
 
-function render(text) {
-    const cols = term.cols();
-    return trim(figlet.textSync(text, {
-        font: font,
-        width: cols,
-        whitespaceBreak: true
-    }));
-}
-
-function trim(str) {
-    return str.replace(/[\n\s]+$/, '');
-}
-
-
-const formatter = new Intl.ListFormat('en', {
-    style: 'long',
-    type: 'conjuction',
-})
